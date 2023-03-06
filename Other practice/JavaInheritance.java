4ad232a91a306e16dafa36f96fb6ca0144784e28
martapalermo <marta.palermo421@gmail.com>
2023-03-06 16:15:18 +0100

    added new folder with additional practice problems

---
+++ Other practice/JavaInheritance.java
@@ -0,0 +1,24 @@
+class Aniaml {
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+
+class Bird extends Aniaml{
+    void fly(){
+        System.out.println("I am flying");
+    }
+
+    void sing(){
+        System.out.println("I am singing");
+    }
+}
+
+class Solution{
+    public static void main(String[] args){
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
