b0a89c0974ae9802fcdbb5f94e98682cb7914c0a
martapalermo <marta.palermo421@gmail.com>
2023-03-07 15:12:06 +0100

    loops I

---
+++ HackerRankProblems/Basic/JavaLoopsI.java
@@ -0,0 +1,14 @@
+package HackerRankProblems.Basic;
+import java.util.*;
+
+class Solution {
+    static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int N = scanner.nextInt();
+        scanner.close();
+        for(int i=1; i <=10; i++){
+            System.out.printf("%d x %d = %d%n", N, i, N*i);
+        }
+    }
+}
---
+++ HackerRankProblems/Basic/JavaStdInStdOut.java
@@ -0,0 +1,26 @@
+// read input stdin 
+// write output stdout 
+// use scanner class + specify input stream as System.in
+
+package HackerRankProblems.Basic;
+
+import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+        scan.close();
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+    }
+}
